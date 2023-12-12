2e32747f3716a76a05dabc0d548b72af281fa1ad
Mike Kistler (from Dev Box) <mikekistler@microsoft.com>
2023-12-12 08:28:00 -0600

    Create test1 for JSON serialization

--- SystemTextJson/Program.cs
+++ SystemTextJson/Program.cs
@@ -21,14 +21,14 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/test1", () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            DateTime.UtcNow.AddDays(index),
             Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
+            index % 2 == 0 ? summaries[Random.Shared.Next(summaries.Length)] : null
         ))
         .ToArray();
     return forecast;
@@ -38,7 +38,7 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+internal record WeatherForecast(DateTimeOffset Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
